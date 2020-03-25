e31664c7724db4f70d0c0b4b299bb73f2294d7f4
Gil Gonçalves <lursty@gmail.com>
2020-03-25 18:40:04 +0000

    Add pollen endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,51 @@
 use actix_web::{web, Result};
-use serde::Serialize;
 use std::env;
+
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct PollenCount {
+    pollenCountInfo: PollenCountInfo,
+}
+
+#[derive(Serialize)]
+struct PollenCountInfo {
+    date: String,
+    index_id: String,
+    index_display_name: String,
+    types: PollenTypes,
+    plants: PlantTypes,
+}
+
+#[derive(Serialize)]
+struct PollenTypes {
+    grass: PollenType,
+    tree: PollenType,
+    weed: PollenType,
+}
+
+#[derive(Serialize)]
+struct PlantTypes {
+    olive: PollenType,
+    graminales: PollenType,
+    ragweed: PollenType,
+    birch: PollenType,
+}
+
+#[derive(Serialize)]
+struct PollenType {
+    display_name: String,
+    in_season: bool,
+    data_available: bool,
+    index: PollenIndex,
+}
+
+#[derive(Serialize)]
+struct PollenIndex {
+    value: Option<u8>,
+    category: Option<String>,
+    color: Option<String>,
+}
 
 #[derive(Serialize)]
 struct AirQuality {
@@ -56,6 +101,11 @@ struct Precipitation {
 struct Wind {
     speed: ValueUnits,
     direction: i32,
+}
+
+#[derive(Serialize)]
+struct Index {
+    urls: Vec<String>,
 }
 
 // https://api.breezometer.com/weather/v1/current-conditions'
@@ -124,16 +174,97 @@ async fn air_quality(_info: web::Path<()>) -> Result<web::Json<AirQuality>> {
     }))
 }
 
-#[derive(Serialize)]
-struct Index {
-    urls: Vec<String>,
+// https://api.breezometer.com/air-quality/v2/current-conditions?'
+async fn pollen_count(_info: web::Path<()>) -> Result<web::Json<PollenCount>> {
+    Ok(web::Json(PollenCount {
+        pollenCountInfo: PollenCountInfo {
+            date: "2020-03-05".to_string(),
+            index_id: "bpi".to_string(),
+            index_display_name: "BreezoMeter Pollen Index".to_string(),
+            types: PollenTypes {
+                grass: PollenType {
+                    display_name: "Grass".to_string(),
+                    in_season: false,
+                    data_available: false,
+                    index: PollenIndex {
+                        value: None,
+                        category: None,
+                        color: None,
+                    },
+                },
+                tree: PollenType {
+                    display_name: "Tree".to_string(),
+                    in_season: true,
+                    data_available: true,
+                    index: PollenIndex {
+                        value: Some(0),
+                        category: Some("None".to_string()),
+                        color: None,
+                    },
+                },
+                weed: PollenType {
+                    display_name: "Weed".to_string(),
+                    in_season: false,
+                    data_available: false,
+                    index: PollenIndex {
+                        value: None,
+                        category: None,
+                        color: None,
+                    },
+                },
+            },
+            plants: PlantTypes {
+                olive: PollenType {
+                    display_name: "Olive".to_string(),
+                    in_season: false,
+                    data_available: false,
+                    index: PollenIndex {
+                        value: None,
+                        category: None,
+                        color: None,
+                    },
+                },
+                graminales: PollenType {
+                    display_name: "Graminales".to_string(),
+                    in_season: false,
+                    data_available: false,
+                    index: PollenIndex {
+                        value: None,
+                        category: None,
+                        color: None,
+                    },
+                },
+                ragweed: PollenType {
+                    display_name: "Ragweed".to_string(),
+                    in_season: false,
+                    data_available: false,
+                    index: PollenIndex {
+                        value: None,
+                        category: None,
+                        color: None,
+                    },
+                },
+                birch: PollenType {
+                    display_name: "Birch".to_string(),
+                    in_season: true,
+                    data_available: true,
+                    index: PollenIndex {
+                        value: Some(0),
+                        category: Some("None".to_string()),
+                        color: None,
+                    },
+                },
+            },
+        },
+    }))
 }
 
 async fn index(_info: web::Path<()>) -> Result<web::Json<Index>> {
     Ok(web::Json(Index {
         urls: [
-            "http://localhost:8080/air-quality/v2/current-conditions".to_string(),
-            "http://localhost:8080/weather/v1/current-conditions".to_string(),
+            "http://localhost:3000/air-quality/v2/current-conditions".to_string(),
+            "http://localhost:3000/weather/v1/current-conditions".to_string(),
+            "http://localhost:3000/pollen/v2/forecast/daily".to_string(),
         ]
         .to_vec(),
     }))
@@ -159,6 +290,7 @@ async fn main() -> std::io::Result<()> {
                 web::get().to(air_quality),
             )
             .route("/weather/v1/current-conditions", web::get().to(weather))
+            .route("pollen/v2/forecast/daily", web::get().to(pollen_count))
     })
     .bind(("0.0.0.0", port))?
     .run()
